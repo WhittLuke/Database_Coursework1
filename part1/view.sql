5e98e09bb2837323021fef33888cf87dc115a537
Luke <32763261+WhittLuke@users.noreply.github.com>
2017-11-12 22:01:57 +0000

    Add files via upload
    Initial commit of View script. Still need to create an insert and update queries

--- part1/view.sql
+++ part1/view.sql
@@ -5,6 +5,13 @@
 
 
 -- add your CREATE VIEW statement here
+-- Want to select projects where the end date is either Dec 2017
+-- or any date in 2018
 
+CREATE VIEW ongoingProjects
+AS SELECT *
+FROM project
+WHERE endDate LIKE '201712%' AND endDate LIKE '2018%'
+WITH CHECK OPTION;
 
 -- add your statement to test rejection
